0b5803144d026d3075f4db4d2eb40b1481767f7c
abdoSobhy1 <abdoosobhy97@gmail.com>
2023-12-03 05:20:38 +0200

    test

---
+++ app/example/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const example = () => {
+  return (
+    <>
+      <div>Example</div>
+      <Link href="/">Home</Link>
+    </>
+  );
+};
+
+export default example;
